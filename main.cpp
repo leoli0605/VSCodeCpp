66d1a052b69cb56c86b1598971ae10ac99ee834b
leoli <jafee201153@gmail.com>
2023-09-27 21:18:52 +0800

    feat: support both Windows and MacOS, and add UtilityClang as a submodule

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
+#include <unistd.h>
+
 #include <iostream>
 
-int main()
-{
+#include "utility_c.h"
+
+int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    statusbar *status = statusbar_new("Indeterminate");
+    for (int i = 0; i < 100; i++) {
+        statusbar_inc(status);
+        sleep(1);
+    }
+    statusbar_finish(status);
     return 0;
 }
